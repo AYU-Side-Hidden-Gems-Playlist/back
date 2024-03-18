48eea6487fe4ea32b2c4107d4af82133ae9b0a7e
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-03-18 17:53:35 +0900

    build.gradle
    AYU-Side-Hidden-Gems-Playlist/back

---
+++ AYU-Side-Hidden Gems Playlist-back/src/test/java/com/example/ayusidehiddengemsplaylistback/AyuSideHiddenGemsPlaylistBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ayusidehiddengemsplaylistback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyuSideHiddenGemsPlaylistBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
