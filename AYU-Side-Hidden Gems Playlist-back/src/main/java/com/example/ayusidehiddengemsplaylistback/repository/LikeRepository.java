6ff8c500f53fd0637306ee696d91d2188f61c05c
ulsae <ulsae0912@gmail.com>
2024-05-02 17:34:48 +0900

    Like api

---
+++ AYU-Side-Hidden Gems Playlist-back/src/main/java/com/example/ayusidehiddengemsplaylistback/repository/LikeRepository.java
@@ -0,0 +1,11 @@
+package com.example.ayusidehiddengemsplaylistback.repository;
+
+import com.example.ayusidehiddengemsplaylistback.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByMemberAndPlaylist(Member member, Playlist playlist);
+}
---
+++ AYU-Side-Hidden Gems Playlist-back/src/main/java/com/example/ayusidehiddengemsplaylistback/service/LikeService.java
@@ -0,0 +1,51 @@
+package com.example.ayusidehiddengemsplaylistback.service;
+
+import com.example.ayusidehiddengemsplaylistback.entity.Like;
+import com.example.ayusidehiddengemsplaylistback.form.LikeForm;
+import com.example.ayusidehiddengemsplaylistback.repository.LikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.ayusidehiddengemsplaylistback.exception.NotFoundException;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+    private final LikeRepository likeRepository;
+    private final MemberRepository memberRepository;
+    private final PlaylistRepository playlistRepository;
+
+    @Transactional
+    public void like(LikeForm likeForm) throws Exception {
+        Member member = memberRepository.findById(likeForm.getMemberId())
+                .orElseThrow(() -> new NotFoundException("Could not found member id : " + likeForm.getMemberId())); //notfoundexception클래스 추가예정
+
+        Playlist playlist = playlistRepository.findById(likeForm.getPlaylistId())
+                .orElseThrow(() -> new NotFoundException("Could not found playlist id : " + likeForm.getPlaylistId())); //동일
+
+        if (likeRepository.findByMemberAndPlaylist(member, playlist).isPresent()){
+            throw new Exception("Like already exists.");
+        }
+
+        Like like = Like.builder()
+                .playlist(playlist)
+                .member(member)
+                .build();
+
+        likeRepository.save(like);
+    }
+
+    @Transactional
+    public void unlike(LikeForm likeForm) {
+        Member member = memberRepository.findById(likeForm.getMemberId())
+                .orElseThrow(() -> new NotFoundException("Miss member id : " + likeForm.getMemberId()));
+
+        Playlist playlist = playlistRepository.findById(likeForm.getPlaylistId())
+                .orElseThrow(() -> new NotFoundException("Miss playlist id : " + likeForm.getPlaylistId()));
+
+        Like like = likeRepository.findByMemberAndPlaylist(member, playlist)
+                .orElseThrow(() -> new NotFoundException("Miss Like Id"));
+
+        likeRepository.delete(like);
+    }
+}
