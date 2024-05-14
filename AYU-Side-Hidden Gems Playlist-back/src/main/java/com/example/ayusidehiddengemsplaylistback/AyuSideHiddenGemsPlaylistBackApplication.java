9707f708d19d5e98e9387a89a7938626e6eb7b45
jangjinhong <honge1122@naver.com>
2024-05-14 23:53:10 +0900

    Application package 경로 수정

--- AYU-Side-Hidden Gems Playlist-back/src/main/java/com/example/ayusidehiddengemsplaylistback/AyuSideHiddenGemsPlaylistBackApplication.java
+++ AYU-Side-Hidden Gems Playlist-back/src/main/java/com/example/ayusidehiddengemsplaylistback/AyuSideHiddenGemsPlaylistBackApplication.java
@@ -1,14 +1,11 @@
-package com.example.ayusidehiddengemsplaylistback;
+package main.java.com.example.ayusidehiddengemsplaylistback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class AyuSideHiddenGemsPlaylistBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AyuSideHiddenGemsPlaylistBackApplication.class, args);
     }
-
 }
