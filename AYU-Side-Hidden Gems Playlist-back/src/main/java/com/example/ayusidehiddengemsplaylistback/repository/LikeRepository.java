77777ae0cff8814b5c61e4acbfb03423c0028a20
MinDi <123048828+alswl0912@users.noreply.github.com>
2024-05-19 23:12:41 +0900

    Member, Playlist entity import

--- AYU-Side-Hidden Gems Playlist-back/src/main/java/com/example/ayusidehiddengemsplaylistback/repository/LikeRepository.java
+++ AYU-Side-Hidden Gems Playlist-back/src/main/java/com/example/ayusidehiddengemsplaylistback/repository/LikeRepository.java
@@ -1,5 +1,7 @@
 package com.example.ayusidehiddengemsplaylistback.repository;
 
+import com.example.ayusidehiddengemsplaylistback.entity.Member;
+import com.example.ayusidehiddengemsplaylistback.entity.Playlist;
 import com.example.ayusidehiddengemsplaylistback.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
